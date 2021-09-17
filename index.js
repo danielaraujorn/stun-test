cefbef102d362fa3b45819e8946ab0fabea67644
Daniel Araújo <daniel.araujorn01@gmail.com>
2021-09-17 09:58:17 -0300

    stun server ready

---
+++ index.js
@@ -0,0 +1,24 @@
+const stun = require("stun");
+
+const server = stun.createServer({ type: "udp4" });
+
+const { STUN_BINDING_RESPONSE, STUN_EVENT_BINDING_REQUEST } = stun.constants;
+const userAgent = `node/${process.version} stun/v1.0.0`;
+
+server.on(STUN_EVENT_BINDING_REQUEST, (request, rinfo) => {
+  console.log("chegou");
+  console.log(rinfo);
+  const message = stun.createMessage(
+    STUN_BINDING_RESPONSE,
+    request.transactionId
+  );
+
+  message.addXorAddress(rinfo.address, rinfo.port);
+  message.addSoftware(userAgent);
+
+  server.send(message, rinfo.port, rinfo.address);
+});
+
+server.listen(process.env.PORT || 19302, () => {
+  console.log("[stun] server started");
+});
